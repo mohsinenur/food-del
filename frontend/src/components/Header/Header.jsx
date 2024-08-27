5958e3549ce0b6f37c2924e17ba28784d77f4155
mohsinenur <follow.mohsin@gmail.com>
2024-08-27 07:55:27 +0600

    header design

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-        
+        <div className='header-contents'>
+          <h2>Order your favorite food here</h2> 
+          <p>Choose from diverse menu featuring a delactable array of dishes</p>
+          <button>View Menu</button>
+        </div>
     </div>
   )
 }
